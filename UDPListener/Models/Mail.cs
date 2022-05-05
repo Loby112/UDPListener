29f8fb36779c1eb21776b49d04f3dc40807b4687
Tobias Lykke Petersen <tobi195h@edu.easj.dk>
2022-05-05 11:59:08 +0200

    Add project files.

---
+++ UDPListener/Models/Mail.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UDPListener.Models
+{
+    public class Mail
+    {
+        public int Id { get; set; }
+        //private DateTime _timeStamp; // lav datetime om til Unix Time og ændre test herefter
+        private Int32 _unixTimestamp;
+        private string _detected;
+        
+        public Int32 UnixTimeStamp
+        {
+            get => _unixTimestamp;
+            set
+            {
+                value = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+                if (value < 1600000000) throw new ArgumentOutOfRangeException("UnixTimeStamp", "Must be more than 1600000000 sec.");
+                _unixTimestamp = value;
+            }
+        }
+
+        public string Detected
+        {
+            get => _detected;
+            set
+            {
+                if (value == null) throw new ArgumentNullException("Detected", "Cannot be null");
+                //if (value != "Detected") throw new ArgumentOutOfRangeException("Detected", "");
+                _detected = value;
+            }
+
+        }
+
+        public Mail()
+        {
+
+        }
+
+        public Mail(int id, Int32 unixTimeStamp, string detected)
+        {
+            Id = id;
+            UnixTimeStamp = unixTimeStamp;
+            Detected = detected;
+        }
+    }
+}
---
+++ UDPListener/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using UDPListener.Models;
+
+namespace UDPListener {
+    internal class Program{
+        private static string URL = "https://yougotmailapi.azurewebsites.net/api/Mail";
+        private static Mail mail = new Mail();
+        static void Main(string[] args) {
+            Console.WriteLine("Goddag");
+            using (UdpClient socket = new UdpClient()){
+                socket.Client.Bind(new IPEndPoint(IPAddress.Any, 7005));
+
+                using (HttpClient client = new HttpClient()){
+                    while (true){
+                        IPEndPoint from = null;
+                        byte[] data = socket.Receive(ref from);
+                        string received = Encoding.UTF8.GetString(data);
+                        mail.UnixTimeStamp = 0;
+                        mail.Id = 0;
+                        mail.Detected = received;
+
+                        string serializedData = JsonSerializer.Serialize(mail);
+                        Console.WriteLine("Server received " + received + " From " + from.Address);
+                        HttpContent content = new StringContent(serializedData, Encoding.UTF8, "text/json");
+                        Console.WriteLine(content.ReadAsStringAsync().Result);
+                        client.PostAsync(URL, content);
+                            
+
+                    }
+                }
+
+            }
+        }
+    }
+}
